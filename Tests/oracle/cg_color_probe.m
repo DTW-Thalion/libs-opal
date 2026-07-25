c4c01b572e2c7065b0462c962d5a01ceaa8ea7bc
Todd White <todd.white@thalion.global>
2026-07-25 10:26:25 -0400

    probe CGColor against Apple CoreGraphics

---
+++ Tests/oracle/cg_color_probe.m
@@ -0,0 +1,44 @@
+/* Apple oracle: CGColor components, alpha, number of components, equality
+   and copy-with-alpha for the generic colour spaces. */
+#import <CoreGraphics/CoreGraphics.h>
+#import <ApplicationServices/ApplicationServices.h>
+#include <stdio.h>
+
+static void dump(const char *name, CGColorRef c)
+{
+  size_t n = CGColorGetNumberOfComponents(c);
+  const CGFloat *comp = CGColorGetComponents(c);
+  printf("%s: n=%zu alpha=%g comps=[", name, n, CGColorGetAlpha(c));
+  for (size_t i = 0; i < n; i++)
+    printf("%s%g", i ? "," : "", comp[i]);
+  printf("]\n");
+}
+
+int main(void)
+{
+  CGColorRef rgb = CGColorCreateGenericRGB(0.1, 0.2, 0.3, 0.4);
+  CGColorRef gray = CGColorCreateGenericGray(0.5, 0.8);
+  CGColorRef cmyk = CGColorCreateGenericCMYK(0.1, 0.2, 0.3, 0.4, 0.5);
+
+  dump("RGB(.1,.2,.3,.4)", rgb);
+  dump("Gray(.5,.8)", gray);
+  dump("CMYK(.1,.2,.3,.4,.5)", cmyk);
+
+  CGColorRef rgb2 = CGColorCreateGenericRGB(0.1, 0.2, 0.3, 0.4);
+  printf("Equal(rgb,rgb2)=%d Equal(rgb,gray)=%d\n",
+    CGColorEqualToColor(rgb, rgb2), CGColorEqualToColor(rgb, gray));
+
+  CGColorRef rgbA = CGColorCreateCopyWithAlpha(rgb, 0.9);
+  dump("CopyWithAlpha(rgb,0.9)", rgbA);
+
+  CGColorRef copy = CGColorCreateCopy(rgb);
+  printf("Equal(rgb,copy)=%d\n", CGColorEqualToColor(rgb, copy));
+
+  CGColorSpaceRef cs = CGColorGetColorSpace(rgb);
+  printf("rgb colorspace model=%d (RGB=%d)\n",
+    (int)CGColorSpaceGetModel(cs), (int)kCGColorSpaceModelRGB);
+
+  CGColorRelease(rgb); CGColorRelease(gray); CGColorRelease(cmyk);
+  CGColorRelease(rgb2); CGColorRelease(rgbA); CGColorRelease(copy);
+  return 0;
+}
