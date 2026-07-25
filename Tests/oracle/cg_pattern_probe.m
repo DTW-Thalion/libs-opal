5871bcdc23d006bf909dc85781d018163774bcad
Todd White <todd.white@thalion.global>
2026-07-25 17:41:20 -0400

    probe CGPattern vertical tiling against Apple CoreGraphics

--- Tests/oracle/cg_pattern_probe.m
+++ Tests/oracle/cg_pattern_probe.m
@@ -25,6 +25,13 @@ static void drawCellUncolored(void *info, CGContextRef c)
   CGContextFillRect(c, CGRectMake(0, 0, 2, 4));
 }
 
+static void drawCellBottom(void *info, CGContextRef c)
+{
+  /* Colored: paint the bottom half (y 0..2) green. */
+  CGContextSetRGBFillColor(c, 0, 1, 0, 1);
+  CGContextFillRect(c, CGRectMake(0, 0, 4, 2));
+}
+
 static int A(unsigned char *d, int x, int y) { return d[(y*W + x)*4 + 3]; }
 static int G(unsigned char *d, int x, int y) { return d[(y*W + x)*4 + 1]; }
 
@@ -48,6 +55,26 @@ int main(void)
     unsigned char *d = CGBitmapContextGetData(c);
     printf("colored row y=4:");
     for (int x = 0; x < 12; x++) printf(" %d/%d", A(d,x,4), G(d,x,4));
+    printf("\n");
+    free(buf);
+  }
+
+  /* Vertical variation: bottom-half cell, to pin the vertical tiling. */
+  {
+    unsigned char *buf = calloc(W*H*4, 1);
+    CGContextRef c = CGBitmapContextCreate(buf, W, H, 8, W*4, dev,
+      kCGImageAlphaPremultipliedLast);
+    CGPatternCallbacks cbb = {0, drawCellBottom, NULL};
+    CGPatternRef pat = CGPatternCreate(NULL, CGRectMake(0, 0, 4, 4),
+      CGAffineTransformIdentity, 4, 4, kCGPatternTilingNoDistortion, 1, &cbb);
+    CGColorSpaceRef pcs = CGColorSpaceCreatePattern(NULL);
+    CGContextSetFillColorSpace(c, pcs);
+    CGFloat alpha = 1;
+    CGContextSetFillPattern(c, pat, &alpha);
+    CGContextFillRect(c, CGRectMake(0, 0, W, H));
+    unsigned char *d = CGBitmapContextGetData(c);
+    printf("vertical col x=1:");
+    for (int y = 0; y < 8; y++) printf(" %d/%d", A(d,1,y), G(d,1,y));
     printf("\n");
     free(buf);
   }
